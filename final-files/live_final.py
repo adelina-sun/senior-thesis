d50df28fc3a65a366077770a729e5f1fc57a9841
adelina-sun <adelina.sun@gmail.com>
2016-05-06 23:27:48 -0400

    Update live_final.py

--- final-files/live_final.py
+++ final-files/live_final.py
@@ -255,7 +255,7 @@ def get_location_ranking(source, current, tweet, flag):
         return 0.25
     else:
         source_coords = get_location_coordinates(source)
-        if tweet == "None":
+        if tweet is None:
             try:
                 dist = vincenty(source_coords, current).miles
             except Exception:
